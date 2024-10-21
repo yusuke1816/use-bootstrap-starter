e84fc2872e77a32fe8d26cfee6e8393e168b0db1
yusuke1816 <ktc24a31g0014@edu.kyoto-tech.ac.jp>
2024-10-21 14:50:49 +0900

    copyadd

---
+++ pages/example2.vue
@@ -0,0 +1,72 @@
+
+
+<template>
+  <div>
+    <div class="container shadow p-3 mt-5 bg-body-tertiary rounded" >
+<h1 class="display-1 mt-5 ">美味しさNo.１！KFC風フライドチキン</h1>
+
+<p class="lead text-center"><mark>KFC人気レシピ1位！</mark>数あるKFCレシピ─どれも何か一味足りない気が。試行錯誤の末の主人も絶賛☆再現率の高いレシピです☆</p>
+</div>
+ 
+  
+  
+  
+  <div class="container text-center shadow p-3 mt-5 bg-body-tertiary rounded ">
+  <div class="row mt-5">
+    <div class="col-12 col-md-4 border px-5 ">
+      <h2>作り方</h2>
+    <ol>
+      <li>☆をボウルに混ぜ合わせておく</li>
+      <li>ボウルに★を全て混ぜ合わせておく。</li>
+      <li>肉の厚みがある部分を切り開き、厚みを一定にする。鶏肉１枚を２～３等分の大きめに切る。</li>
+      <li>１のボウルに鶏肉を浸す。(時間があれば20分以上浸す。)</li>
+      <li>２のボウルに鶏肉を入れて粉類をまんべんなくまぶし、別の容器に並べていく。粉が余るようでしたら、二度付けをお勧めします！</li>
+      <li>卵液→粉→卵液→粉と二度付けする事で、カリっと感がアップ＋味もしっかり付きます！写真の我が家のは二度付けしています。</li>
+      <li>油を160度に温める。(温めるのはこのタイミングで大丈夫です。待ってる間に衣が鶏肉になじみます。)</li>
+      <li>160度で５分以上、様子を見ながら、火が通ったら、一度上げて、今度は180～200度でカリっと仕上げ、完成です☆</li>
+      <li>鶏肉の1枚はサイズにばらつきがある為、2～3枚という記載に変更しました。</li>
+    </ol>
+    </div>
+    
+    <div class="col-12 col-md-4 border px-5 ">
+      <h2>コツ・ポイント</h2>
+  <p>揚げる際鍋底での焦げ防止に衣が軽く揚がるまで菜箸等で時々浮かせて下さい</p>
+  <p>好みで粉類に粗びき黒胡椒小さじ1/2を足しても◎</p>
+  <p>にんにくチューブ２センチはガーリックパウダー小さじ１に変えて粉類と混ぜても美味しいです。</p>
+  <p>パプリカパウダーは無くても可</p>
+    </div>
+    
+    <div class="col-12 col-md-4 border px-5 ">
+      <h2>材料</h2>
+<ul class="list-unstyled">
+  <li>鶏肉(お好きな部位)　２〜３枚位</li>
+  <li>☆牛乳　1/2カップ</li>
+  <li>☆卵　１個</li>
+  <li>☆にんにくチューブ　２センチ位</li>
+  <li>★薄力粉　１カップ</li>
+  <li>★オールスパイス　小さじ１弱</li>
+  <li>★顆粒コンソメ　大さじ1/2</li>
+  <li>★塩　大さじ1/2</li>
+  <li><del>あれば★パプリカパウダー　小さじ１</del></li>
+</ul>
+    </div>
+    </div>
+ </div>
+
+<div class=" container "   >
+  <h2 class="badge bg-primary text-wrap" >このレシピの生い立ち</h2>
+   <p>今まで作ったKFCレシピは、卵液や粉もかなり余るし、何よりも</p>
+   <p>何かひと味足りない……と思って、やっとKFC味にたどり着きました！</p>
+   <p>どの家庭にもあるような材料で☆</p>
+   <p>牛乳と卵液、粉類も必要量のみになっているので無駄が無く経済的です☆</p>
+</div>
+  </div>
+ 
+</template>
+
+
+<script>
+export default {
+  // Your component logic goes here
+}
+</script>
